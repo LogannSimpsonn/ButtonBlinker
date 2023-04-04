2708800928e12fadb425383fac941b64c2bd3810
LogannSimpsonn <lns6243@nyu.edu>
2023-04-04 12:13:28 -0400

    added button

--- ButtonBlinker.ino
+++ ButtonBlinker.ino
@@ -1,9 +1,10 @@
+int buttonPin = 32;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
